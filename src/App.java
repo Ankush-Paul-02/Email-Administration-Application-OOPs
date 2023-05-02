ece827b6de5937a4976f098057634cd4dbd417e7
Ankush <ankushpaulclg2002@gmail.com>
2023-05-03 02:15:08 +0530

    Third commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,12 @@
 public class App {
     public static void main(String[] args) {
         Email email = new Email("Ankush", "Paul");
-        
+
+        email.setAlternateEmail("trex.gmail.com");
+
+        System.out.println("Your alternate email is: " + email.getAlternateEmail());
+
+        System.out.println(email);
+
     }
 }
--- src/Email.java
+++ src/Email.java
@@ -2,28 +2,30 @@ import java.util.*;
 
 public class Email {
     static Scanner sc = new Scanner(System.in);
-    private String firstName, lastName, password, department, alternateEmail;
-
-    private int mailBoxCapacity, defaultPasswordLength = 10;
+    private String firstName, lastName, password, department, email, alternateEmail;
+    private String companySuffix = "teamfuture.in";
+    private int mailBoxCapacity = 500, defaultPasswordLength = 10;
 
     /**
      * Constructor
      * 
-     * @param firstName  receive first name and set
-     * @param lastName   receive last name and set
+     * @param firstName receive first name and set
+     * @param lastName  receive last name and set
      */
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("Email created: " + this.firstName + " " + this.lastName);
-        
-        //! Call a method asking for department
-        this.department = setDepartment();
-        System.out.println("Department: " + this.department);
 
-        //! Call a method that returns a random password
+        // ! Call a method asking for department
+        this.department = setDepartment();
+
+        // ! Combine elements to generate email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "." + department + "@" + companySuffix;
+
+        // ! Call a method that returns a random password
         this.password = randomPassword(defaultPasswordLength);
         System.out.println("Password: " + this.password);
+
     }
 
     /**
@@ -31,6 +33,7 @@ public class Email {
      */
     private String setDepartment() {
         System.out.println("DEPARTMENT CODE\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+        System.out.print("Enter the department code: ");
         int departmentChoice = sc.nextInt();
         if (departmentChoice == 1) {
             return "sales";
@@ -43,7 +46,6 @@ public class Email {
         }
     }
 
-
     /**
      * @param length of the password
      * @return a random password
@@ -53,9 +55,60 @@ public class Email {
 
         char[] password = new char[length];
         for (int i = 0; i < length; i++) {
-            int rand = (int)(Math.random() * passwordSet.length());
+            int rand = (int) (Math.random() * passwordSet.length());
             password[i] = passwordSet.charAt(rand);
         }
         return new String(password);
     }
+
+    /**
+     * @param capacity set to mailBox capacity
+     */
+    public void setMailBoxCapacity(int capacity) {
+        this.mailBoxCapacity = capacity;
+    }
+
+    /**
+     * @param alternateEmail set it to alternate email
+     */
+    public void setAlternateEmail(String alternateEmail) {
+        this.alternateEmail = alternateEmail;
+    }
+
+    /**
+     * @param password change and set in the password
+     */
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return mailbox capacity
+     */
+    public int getMailBoxCapacity() {
+        return mailBoxCapacity;
+    }
+
+    /**
+     * @return alternate email
+     */
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return "Name: " + firstName + " " + lastName + ", Email: " + email + ", Department: " + department + ", Mailbox capacity: " + mailBoxCapacity;
+    }
+
 }
